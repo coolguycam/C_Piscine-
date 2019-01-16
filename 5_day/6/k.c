afc930c41aebaaa32368f89ccde66afb8af99993
Cameron Dimitroff <cdimitro@e1z1r6p40.42.us.org>
2019-01-15 23:14:24 -0800

    ex6

--- 5_day/6/k.c
+++ 5_day/6/k.c
@@ -2,21 +2,30 @@
 
 int        ft_strcmp(char *s1, char *s2)
 {
-    while (s1 == s2 && s1 != '\0')
+    int i;
+
+    i = 0;
+    while (s1)
     {
-        s1++;
-        s2++;
+        while (s1[i] == s2[i] && s1[i] != '\0')
+        {
+            s1++;
+            s2++;
+            i++;
+        }
+        if (s1[i] != s2[i])
+            return ((s1 > s2) - (s1 < s2));
+        else
+            return (0);
     }
-    return ((s1 > s2) - (s1 < s2));
 }
 
 int main() {
-  
   char str1[5] = "helo";
-  char str2[7] = "ketauy";
+  char str2[5] = "helo";
   printf("%s\n", str1);
   printf("%s\n", str2);
   
   printf("%d\n", ft_strcmp(str1, str2));
-  return 0;
+  return (0);
 }
