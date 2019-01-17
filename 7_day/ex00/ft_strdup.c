2036bc84407f46cce35502a27c283533287df967
Cameron Dimitroff <cdimitro@e1z1r6p40.42.us.org>
2019-01-17 13:29:46 -0800

    done

--- 7_day/ex00/ft_strdup.c
+++ 7_day/ex00/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: cdimitro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/16 23:03:06 by cdimitro          #+#    #+#             */
-/*   Updated: 2019/01/17 11:23:26 by cdimitro         ###   ########.fr       */
+/*   Updated: 2019/01/17 13:29:30 by cdimitro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,14 @@ char	*ft_strdup(char *str)
 	i = 0;
 	dup = (char*)malloc(ft_strlen(str) + 1);
 	while (str[i] != '\0')
+	{
 		dup[i] = str[i];
+		i++;
+		if (!str[i])
+		{
+			break ;
+		}
+	}
 	dup[i] = str[i];
 	return (dup);
 }
