51bfd4e5bf5eeb964d8a24b9cb4a1df6e298af18
Cameron Dimitroff <cdimitro@e1z1r6p40.42.us.org>
2019-01-18 22:31:05 -0800

    fuck

---
+++ 8_day/ex01/ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cdimitro <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/18 13:51:48 by cdimitro          #+#    #+#             */
+/*   Updated: 2019/01/18 14:38:26 by cdimitro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_putstr(char *c);
+int		ft_strlen(char *c);
+void	ft_swap(int *a, int *b);
+void	ft_putnbr(int nb);
+
+#endif
---
+++ 8_day/ex02/ft_boolean.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cdimitro <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/18 15:23:44 by cdimitro          #+#    #+#             */
+/*   Updated: 2019/01/18 16:09:40 by cdimitro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+# include <unistd.h>
+# define EVEN(x) ((x % 2) == 0)
+# define TRUE 1
+# define FALSE 0
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+# define SUCCESS 0
+
+typedef int		t_bool;
+#endif
